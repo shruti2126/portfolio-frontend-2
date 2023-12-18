7333f90a4bcf951d1c0f94820524940d0511c630
Shruti <ss.sharma1826@gmail.com>
2023-12-17 23:59:37 -0600

    email change

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@
 // index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
